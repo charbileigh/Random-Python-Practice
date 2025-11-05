b275c6f015800a07043191062292acdfc0e2aba2
charbileigh <chabi.seopa@gmail.com>
2025-11-05 11:17:57 +0200

    Initial commit

---
+++ Basic_List_Operations.py
@@ -0,0 +1,33 @@
+print("This is a mini list operation project")
+print()
+print("Ensure that you enter a list with a minimum of 4 entries")
+print()
+print("For example: [a, b, c, d]")
+print()
+
+list_input = input("Enter a list of your choice: ")
+
+'''
+    This is a mini python project that performs basic list / array operations for any list given.
+    All of these operations are manual and stray away from the build-in features.
+    This is because, the project aims to help a user identify data structure patterns early on.
+'''
+
+# Finds the largest number in a given list
+
+
+# Finds the smallest number in a given list
+
+
+# Sums all the elements within a given list
+
+# Counts the number of occurrences of an elements within a list
+
+
+# Reverses a list
+ 
+
+# Checks if a list is a palindrome
+
+
+# Removes duplicates from a list
