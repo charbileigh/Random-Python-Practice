b77f23bcdd6678d0de299cb0092cbc574101dc30
charbileigh <chabi.seopa@gmail.com>
2025-11-05 10:57:46 +0200

    Create Lists.py

---
+++ Lists.py
@@ -0,0 +1,8 @@
+print("This is a mini list operation project")
+print()
+print("Ensure that you enter a list with a minimum of 4 entries")
+print()
+print("For example: [a, b, c, d]")
+print()
+
+list_input = input("Enter a list of your choice: ")
