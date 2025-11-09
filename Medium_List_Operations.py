22c197b240aefd1f06771b8ecc9a40c48666bd18
charbileigh <chabi.seopa@gmail.com>
2025-11-09 19:02:42 +0200

    Update Medium_List_Operations.py

--- Medium_List_Operations.py
+++ Medium_List_Operations.py
@@ -25,5 +25,46 @@ nums2 = input("\n Enter the second list of your choice (like [3,7,2]) : ")
 nums2 = ast.literal_eval(nums2)
 
 # Validate a mountain array for nums1 
+def is_mountain(nums1):
+    n = len(nums1)
+    if n < 3: 
+        return "\n This list is not a mountain array"
+
+    i = 0
+    # climb up
+    while i + 1 < n and nums1[i] < nums1[i + 1]:
+        i += 1
+
+    # peak cannot be first or last
+    if i == 0 or i == n - 1:
+        return "\n This list is not a mountain array"
+
+    # climb down
+    while i + 1 < n and nums1[i] > nums1[i + 1]:
+        i += 1
+
+    return "\n This list is a mountain array"
+print(is_mountain(nums1))
 
 # Validate a mountain array for nums2
+def is_mountain(nums2):
+    
+    n = len(nums2)
+    if n < 3: 
+        return False
+
+    i = 0
+    # climb up
+    while i + 1 < n and nums2[i] < nums2[i + 1]:
+        i += 1
+
+    # peak cannot be first or last
+    if i == 0 or i == n - 1:
+        return False
+
+    # climb down
+    while i + 1 < n and nums2[i] > nums2[i + 1]:
+        i += 1
+
+    return "\n This list is a mountain array"
+print(is_mountain(nums2))
